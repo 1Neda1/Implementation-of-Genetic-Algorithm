8437cc12d10088f5c9a25b0b1cf4479bb86a2dfc
Neda Esfehani <107118340+NedaaEsf@users.noreply.github.com>
2024-03-15 15:38:29 +0330

    Update bpProject.py

--- bpProject.py
+++ bpProject.py
@@ -1,27 +1,27 @@
 import Gene
 
 #define an object of class Gene
-sina = Gene.Gene()
+Var = Gene.Gene()
 
 #main program
-pool = sina.population()
+pool = Var.population()
 #print(pool)
 keys = sorted(list(pool.keys()))
 #print(keys)
 parent1 = pool[keys[-1]]
 parent2 = pool[keys[-2]]
 #print(parent1, parent2)
-offcross = sina.crossover(parent1, parent2)
+offcross = Var.crossover(parent1, parent2)
 #print(offcross)
-offmute1 = sina.mutation(parent1)
+offmute1 = Var.mutation(parent1)
 #print(offmute1)
-offmute2 = sina.mutation(parent2)
+offmute2 = Var.mutation(parent2)
 #print(offmute2)
 
-#a loop to find how many mutations is needed to create offspring from its parents
+#a loop to find how many mutations are needed to create offspring from its parents
 while True:
-    offcross = sina.crossover(parent1, parent2)
-    if offcross == sina.samp:
+    offcross = Var.crossover(parent1, parent2)
+    if offcross == Var.samp:
         print("Crossover!")
         print("Parents are = ", parent1, " and ", parent2)
         print("Offspring is = ", offcross )
@@ -29,8 +29,8 @@ while True:
     else:
         sa = offcross
         count = 0
-        while offcross != sina.samp:
-            offcross = str(sina.mutation(offcross))
+        while offcross != Var.samp:
+            offcross = str(Var.mutation(offcross))
             count += 1
         print("Mutations!")
         print("Parents are = ", parent1, " and ", parent2)
